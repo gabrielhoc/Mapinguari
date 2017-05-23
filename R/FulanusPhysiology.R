a078507728d5aab54a17cf140adc84fab5892501
Gabriel Caetano <gabrielhoc@gmail.com>
2017-05-23 09:25:40 -0700

    Updting package dependencies

--- R/FulanusPhysiology.R
+++ R/FulanusPhysiology.R
@@ -1,12 +1,17 @@
-#' Prices of 50,000 round cut diamonds.
+#' Running speed achieved by 125 *Fulanus beltranus* lizards in 681 trials under different temperatures.
 #'
-#' A dataset containing the prices and other attributes of almost 54,000
-#' diamonds.
+#' A dataset containing the running speed of *Fulanus beltranus* lizards under different temperatures, the temperatures of the runs, individual identities for each lizard, body size of each individual, the tempetature they were acclimated to, the site where they were captured, their hydration state during the trial.
 #'
-#' @format A data frame with 53940 rows and 10 variables:
+#' @format A data frame with 681 rows and 8 variables:
 #' \describe{
-#'   \item{price}{price, in US dollars}
-#'   \item{carat}{weight of the diamond, in carats}
+#'   \item{species}{price, in US dollars}
+#'   \item{id}{weight of the diamond, in carats}
+#'   \item{temp}{price, in US dollars}
+#'   \item{performance}{weight of the diamond, in carats}
+#'   \item{size}{weight of the diamond, in carats}
+#'   \item{site}{weight of the diamond, in carats}
+#'   \item{acctemp}{weight of the diamond, in carats}
+#'   \item{hydration}{hydration station during trial, in carats}
 #'   ...
 #' }
 #' @source \url{http://www.diamondse.info/}
