2bb456918a8889ad79f8c6b6e95212c493bb3ede
Gabriel Caetano <gabrielhoc@gmail.com>
2017-08-11 16:56:36 -0700

    updated documentation

---
+++ R/Mapinguari.R
@@ -0,0 +1,26 @@
+#' Mapinguari: Tools for process-based biogeographical analysis.
+#'
+#' Mapinguari provides solutions for incorporating biological processes in biogeographical analysis.
+#'
+#' @section Mapinguari functions:
+#'
+#'\itemize{
+#'   \item \code{\link{clean_points}}
+#'   \item \code{\link{fit_curves}}
+#'   \item \code{\link{get_rasters}}
+#'   \item \code{\link{pseudoabsences}}
+#'   \item \code{\link{qlist}}
+#'   \item \code{\link{transform_rasters}}
+#'   \item \code{\link{daylengthFUN}}
+#'   \item \code{\link{AETFUN}}
+#'   \item \code{\link{sradFUN}}
+#'   \item \code{\link{rhFUN}}
+#'   }
+#'
+#' @docType package
+#' @name Mapinguari
+NULL
+
+.onUnload <- function(libpath) {
+  library.dynam.unload("Mapinguari", libpath)
+}
