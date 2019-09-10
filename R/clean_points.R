96b2822524029cd515d1cc1617235b191072815c
Gabriel Caetano <gabrielhoc@gmail.com>
2019-09-10 15:27:11 -0300

    dont run example clean_points

--- R/clean_points.R
+++ R/clean_points.R
@@ -13,7 +13,7 @@
 #' @return Data frame with remaining longitudes and latitudes.
 #'
 #' @examples
-#'
+#'\dontrun{
 #' #First, we need to obtain an altitude raster to filter by altitude.
 #' library(raster)
 #' alt <- raster::getData("alt", country = "BRA", mask = TRUE)
@@ -23,7 +23,7 @@
 #'   clean_points(coord = TtorquatusDistribution,
 #'                merge_dist = 20000,
 #'                filter_layer = !is.na(alt))
-#'
+#'}
 #' @export
 
 clean_points <- function(coord,
